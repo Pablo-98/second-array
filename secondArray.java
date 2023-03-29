ed39f5f4b56bb8127a2162ae497e4a675a9ac596
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-03-29 12:48:06 -0600

    added methods

--- secondArray.java
+++ secondArray.java
@@ -18,17 +18,36 @@ public class secondArray{
 
         //the purpose of this loop is to populate the array with user input 
         for (int i = 0; i < array.length; i++){
-             System.out.println("enter value"); //ask user
+             System.out.println("enter values, enter 0 to stop"); //ask user
              int n = scnr.nextInt(); //extract data from user
              array[i] = n; // store user data into array position i
         }
+        int sum = getSum(array);
+        System.out.println("the sum is " + sum);
+        double avg = getAVG(array); 
+        System.out.println("the avg is " + avg);
 
     } 
     // how many arguments will this take?
     public static int getSum(int[] a) {
         int sum = 0;
+        //for loops and arrays are bffs!
+        for(int i= 0; i< a.length; i++){
+            sum += a[i]; //accumulate
+        }
         return sum;
 
     }
 
-}
+    public static double getAVG(int[] a) {
+        int sum = 0;
+        //for loops and arrays are bffs!
+        for(int i= 0; i< a.length; i++){
+            sum += a[i]; //accumulate
+        } 
+        return ((double)sum)/((double)a.length); //typecast
+
+
+    }
+
+} 
